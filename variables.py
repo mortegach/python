02d6f4d4c271c9175baae24342f82f67ae653991
Marvin Ortega <isaiasmarvin@gmail.com>
2015-02-03 21:53:44 -0600

    First commit. Basic variable types

---
+++ variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2
+
+a,b,c=1,10.3,"Juan"; #Single line multi variable declaration & assignation
+
+print a;
+print b;
+print c;
+
+del c; # deleting a reference 
+
+c="Marvin";
+
+print c;
