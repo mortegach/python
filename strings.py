87a976020c2b48345dc5597ab617d158a48023a1
Marvin Ortega <isaiasmarvin@gmail.com>
2015-02-03 22:00:15 -0600

    Python Strings type exercise, incuding slicing operation

---
+++ strings.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python2
+
+str = "Hello World!\n";
+
+print str; #Prints complete string
+print str[0]; #Prints first character of the string
+print str[2:5]; #Prints characters starting from 3rd to 5th
+print str[2:]; #Prints starting from 3rd char
+print str *2; #Prints string two times
+print str + "TEST"; #Prints concatenated string
+
